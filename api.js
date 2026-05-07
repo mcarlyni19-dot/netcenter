4881fca4a4905ccc94da25a302ac85e2ed5959af
mariacarlyni-pereira <maria.carlyni@academico.ifpb.edu.br>
2026-05-07 13:41:23 +0000

    att

--- api.js
+++ api.js
@@ -1,16 +1,8 @@
-// api.js
-
 export async function buscarFerramentas() {
-    // Pega a URL do Codespaces e aponta para a porta 3000 do JSON Server
-    const apiUrl = window.location.origin.replace('5500', '3000') + '/ferramentas';
-    
     try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) throw new Error('Falha na comunicação com a API');
-        
-        return await response.json();
+        const response = await fetch('./db.json'); // Caminho relativo
+        return await response.json().then(data => data.ferramentas);
     } catch (erro) {
-        console.error("Erro no Fetch:", erro);
-        throw erro; // Repassa o erro para o arquivo principal tratar
+        console.error("Erro ao ler db.json:", erro);
     }
 }
